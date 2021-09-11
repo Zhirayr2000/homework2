9954ed57b6582b8649cb03e46179ae760d177425
Zhirayr2000 <zhirayrtovmasyan2000@gmail.com>
2021-09-11 10:20:02 +0400

    All the mentioned issues have been fixed.

--- src/Cars.java
+++ src/Cars.java
@@ -2,7 +2,7 @@ public class Cars {
     public static void main(String[] args) {
         boolean redIsHitFromBehind=false;
         boolean blackIsHitFromBehind=true;
-        if(redIsHitFromBehind && blackIsHitFromBehind || !redIsHitFromBehind && !blackIsHitFromBehind)
+        if(redIsHitFromBehind==blackIsHitFromBehind)
             System.out.println("Invalid state");
         else if(redIsHitFromBehind)
             System.out.println("Black");
--- src/Chars.java
+++ src/Chars.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Chars {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
         char ch1='S';
         char ch2='T';
         char ch3='v';
-        if(Character.toUpperCase(ch1)>=Character.toUpperCase(ch2))
+        if((ch1>=97?ch1-32:ch1)>(ch2>=97?ch2-32:ch2))
         {
-            if(Character.toUpperCase(ch1)>=Character.toUpperCase(ch3))
+            if((ch1>=97?ch1-32:ch1)>=(ch3>=97?ch3-32:ch3))
                 System.out.println(ch1);
             else
                 System.out.println(ch3);
         }
-        else if(Character.toUpperCase(ch2)>=Character.toUpperCase(ch3))
+        else if((ch2>=97?ch2-32:ch2)>=(ch3>=97?ch3-32:ch3))
             System.out.println(ch2);
         else
             System.out.println(ch3);
